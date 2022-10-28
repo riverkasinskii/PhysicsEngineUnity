a757c39026d01cb3eaa68b7d355d5db161245759
riverkasinskii <kinderplus8@gmail.com>
2022-10-28 16:21:40 +0300

    FirstTask

---
+++ Assets/Scripts/SupermanController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SupermanController : MonoBehaviour
+{
+    [SerializeField] private Vector3 _speed;
+    [SerializeField] private Vector3 Power;    
+
+    private void FixedUpdate()
+    {        
+        GetComponent<Rigidbody>().velocity = _speed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {        
+        if (collision.rigidbody == true)
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            collision.rigidbody.AddForce(Power, ForceMode.Impulse);
+            Debug.Log($"Мы столкнулись с {collision.gameObject.name}");            
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
